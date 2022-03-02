1770e9eda24c073ccb4b2929b6d31d4c9c914c7f
Oscar Fonts <oscar.fonts@geomati.co>
2022-03-02 14:01:24 +0100

    update geocomponents version

--- src/components/SidePanelContent.js
+++ src/components/SidePanelContent.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import Link from '@mui/material/Link';
 import Stack from '@mui/material/Stack';
 import Logo_geomatico from '../img/Logo_geomatico.png';
-import Switchpad from '@geomatico/geocomponents/Switchpad';
+import SwitchPad from '@geomatico/geocomponents/SwitchPad';
 import SearchBox from '@geomatico/geocomponents/SearchBox';
 
 import SectionTitle from './SectionTitle';
@@ -13,10 +13,10 @@ const SidePanelContent = ({categories, selectedCategories, onChangeSelectedCateg
   return <Stack sx={{height: '100%'}}>
 
     <SectionTitle titleKey='Cerca'/>
-    <SearchBox text={searchText} onTextChange={setSearchText} dense={true} onSearchClick={setSearchText} placeholder='Cerca...'></SearchBox>
+    <SearchBox text={searchText} onTextChange={setSearchText} dense={true} onSearchClick={setSearchText} placeholder='Cerca...'/>
 
     <SectionTitle titleKey='Selecciona categoria'/>
-    <Switchpad categories={categories} selected={selectedCategories} onSelectionChange={onChangeSelectedCategories}></Switchpad>
+    <SwitchPad categories={categories} selected={selectedCategories} onSelectionChange={onChangeSelectedCategories}/>
 
     <Link style={{flexGrow: 2, position: 'relative', minHeight: 25}} href='https://geomatico.es' target='_blank'>
       <img src={Logo_geomatico} width={80} alt='geomatico.es' style={{position: 'absolute', bottom: 0, right: 0}}/>
